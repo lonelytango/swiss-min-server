c2b6641e14b580aaad8b84fe70c7549d5d8a28e6
Zian Chen <doomon@gmail.com>
2025-01-09 23:17:21 -0500

    feat: update

--- java/Server.java
+++ java/Server.java
@@ -4,18 +4,17 @@ import java.net.InetSocketAddress;
 
 public class Server {
     public static void main(String[] args) throws Exception {
+        
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        
         server.createContext("/", exchange -> {
-           String request = exchange.getRequestURI().toString();
-           System.out.println("Received request: " + request);
-
             byte[] response = "Welcome to Java Server".getBytes();
             exchange.sendResponseHeaders(200, response.length);
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response);
             }
-            System.out.println("Sent response for: " + request);
         });
+
         server.start();
         System.out.println("Server started on port 8000");
     }
